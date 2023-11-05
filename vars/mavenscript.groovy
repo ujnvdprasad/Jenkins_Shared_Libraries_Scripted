96e8115b71020a0c7b83b804a7d0dad12a01a3af
ujnvdprasad <124246569+ujnvdprasad@users.noreply.github.com>
2023-11-05 21:31:11 +0530

    Update mavenscript.groovy

--- vars/mavenscript.groovy
+++ vars/mavenscript.groovy
@@ -1,12 +1,12 @@
-def MavenHome= tool name: 'Maven-3.9.5'
 def call(String mavenCommand) {
+  def MavenHome= tool name: 'Maven-3.9.5'
   if ("${mavenCommand}" == "Clean") {
     sh "${MavenHome}/bin/mvn clean"
   }
   else if ("${mavenCommand}" == "Test") {
     sh "${MavenHome}/bin/mvn clean test"
   }
-  else if ("${mavenCommand}" == "compile") {
+  else if ("${mavenCommand}" == "Compile") {
     sh "${MavenHome}/bin/mvn clean compile"
   }
   else if ("${mavenCommand}" == "Package") {
