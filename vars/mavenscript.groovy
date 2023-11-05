7f40c58756f672f2f14d4d4ba4263171461d796d
ujnvdprasad <124246569+ujnvdprasad@users.noreply.github.com>
2023-11-05 20:58:05 +0530

    Create mavenscript.groovy

---
+++ vars/mavenscript.groovy
@@ -0,0 +1,17 @@
+def call(String mavenCommand) {
+  if ("${mavenCommand}" == "Clean") {
+    sh "${MavenHome}/bin/mvn clean"
+  }
+  else if ("${mavenCommand}" == "Test") {
+    sh "${MavenHome}/bin/mvn clean test"
+  }
+  else if ("${mavenCommand}" == "compile") {
+    sh "${MavenHome}/bin/mvn clean compile"
+  }
+  else if ("${mavenCommand}" == "Package") {
+    sh "${MavenHome}/bin/mvn clean package"
+  }
+  else if ("${mavenCommand}" == "Install") {
+    sh "${MavenHome}/bin/mvn clean install"
+  }
+}
